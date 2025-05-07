49f98276790d925e6302c04277bdb066f4627c82
Daniela <20124@aesmi.pt>
2025-05-07 12:27:59 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'meu-app-v0';
+const ASSETS = [
+    './',
+    './index.html',
+    './style.css',
+    './script.js',
+    './manifest.json',
+    './cientifico.html',
+    './info.html',
+    './profissionais.html',
+    './profissionaisturismo.html',
+    './icons/icon-192.png',
+    './icons/icon-512.png',
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
